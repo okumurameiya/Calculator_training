514d895af1bb1c189ba83415e590cb0f9e80663f
okumurameiya <105775374+okumurameiya@users.noreply.github.com>
2022-05-24 15:47:55 +0900

    計算機能追加
    入力された文字列を計算式に変換し結果を出力する機能を追加

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 
+// 電卓テスト
+function CalcTest(){
+
+    // 入力された文字列を取得
+    let input = document.getElementById(`fomula`);
+
+    let fomula = input.value;
+    //inputText.item(1).value = null; // 入力欄クリア
+
+    // 文字列を計算式に変換
+    let result = Function('return ('+fomula+');')();
+ 
+    console.log(result);
+
+    var output = document.getElementById(`result`);
+    output.firstChild.nodeValue = result;
+}
