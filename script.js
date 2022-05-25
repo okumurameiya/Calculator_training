1e207394ceadc9c648d8e5f8f74a57303d794954
okumurameiya <105775374+okumurameiya@users.noreply.github.com>
2022-05-25 16:58:07 +0900

    UI実装

--- script.js
+++ script.js
@@ -1,18 +1,42 @@
+/*
+function KeyInput(){
+    let 
+}*/
+let input;
+let fomula;
+let result;
 
-// 電卓テスト
-function CalcTest(){
+// キー入力された文字列を取得
+function Input(){
+    input = document.getElementById(`input`).value;
+    fomula = input;
+    result = Calc(fomula);
+    ShowResult(fomula, result);
+}
 
-    // 入力された文字列を取得
-    let input = document.getElementById(`fomula`);
-
-    let fomula = input.value;
-    //inputText.item(1).value = null; // 入力欄クリア
-
-    // 文字列を計算式に変換
-    let result = Function('return ('+fomula+');')();
  
-    console.log(result);
-
+// 結果表示;
+function ShowResult(){
     var output = document.getElementById(`result`);
-    output.firstChild.nodeValue = result;
+    output.firstChild.nodeValue = `${fomula} = ${result}`;
+}
+
+// 文字列を計算式に変換
+function Calc(){
+     return Function('return ('+fomula+');')();
+}
+
+// 入力欄クリア
+function ClearInput(){
+    input = null; 
+}
+
+// 平方根テスト
+function Root(fomula){
+    let regexp = /√\(.+?\)/g;   // 検索：√(***)
+    let replaced
+    if (fomula.match(regexp) != null){
+     
+    }
+    return replaced;
 }
